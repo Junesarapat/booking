ce81f14facb1b35676857c7682069958748fd19c
Junesarapat <50126397+Junesarapat@users.noreply.github.com>
2019-04-30 11:55:36 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,27 +1,34 @@
-<html>
-<head>
-<title>Login Page</title>
-</head>
-<body>
-<form name="form1" method="post" action="check_login.php">
+<?php
+
+if(!isset($_SESSION)){
+  session_start();
+}
+
+if(isset($POST)){
+  include 'dbconnect.php'; //if already created dbconnect.php to connect to my DB
+  $user = $POST['username];
+  $pass = $POST['password];
+}
+
+
+<form name="form" method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
   Login<br>
   <table border="1" style="width: 300px">
     <tbody>
       <tr>
         <td>Username</td>
         <td>
-          <input name="txtUsername" type="text" id="txtUsername">
+          <input name="username" type="text">
         </td>
       </tr>
       <tr>
         <td>Password</td>
-        <td><input name="txtPassword" type="password" id="txtPassword">
+        <td><input name="password" type="password">
         </td>
       </tr>
     </tbody>
   </table>
   <br>
   <input type="submit" name="Submit" value="Login">
-</form>
-</body>
-</html>
+
+?>
